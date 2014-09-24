caec313cf24ff6d2c53efe496d429bc29835feed
paschke <stefan.paschke@gmail.com>
2014-09-24 08:56:38 +0200

    added parentRole parameter to RoleProvider::findRole

--- Security/RoleProvider.php
+++ Security/RoleProvider.php
@@ -32,31 +32,53 @@ class RoleProvider
      * @param string $name
      * @return string
      */
-    public function findRole($name)
+    public function findRole($name, $parentRole = null)
     {
-        $query = <<<QUERY
-            SELECT r.name AS role
-            FROM {$this->options['role_table_name']} as r
-            WHERE r.name = ?
+        if ($parentRole) {
+            $query = <<<QUERY
+                SELECT r.name AS role
+                FROM {$this->options['role_table_name']} as r
+                LEFT JOIN {$this->options['role_table_name']} AS p ON r.parent_id = p.id
+                WHERE r.name = ? AND p.name = ?
 QUERY;
-        return $this->connection->fetchColumn($query, array($name), 0);
+
+            return $this->connection->fetchColumn($query, array($name, $parentRole), 0);
+        } else {
+            $query = <<<QUERY
+                SELECT r.name AS role
+                FROM {$this->options['role_table_name']} as r
+                WHERE r.name = ?
+QUERY;
+
+            return $this->connection->fetchColumn($query, array($name), 0);
+        }
     }
 
     public function createRole($name, $parentRole = null)
     {
-        $queryParent = <<<QUERY
+        $query = <<<QUERY
             SELECT r.id AS id
             FROM {$this->options['role_table_name']} as r
             WHERE r.name = ?
 QUERY;
 
+        $id = $this->connection->fetchColumn($query, array($name), 0);
+
         if ($parentRole) {
-            $parentId = $this->connection->fetchColumn($queryParent, array($parentRole), 0) ?: null;
+            $parentId = $this->connection->fetchColumn($query, array($parentRole), 0) ?: null;
         } else {
             $parentId = null;
         }
 
-        $this->connection->insert($this->options['role_table_name'], array('name' => $name, 'parent_id' => $parentId));
+        if ($id) {
+            if ($parentId) {
+                $this->connection->update($this->options['role_table_name'], array('name' => $name, 'parent_id' => $parentId), array('id' => $id));
+            } else {
+                $this->connection->update($this->options['role_table_name'], array('name' => $name), array('id' => $id));
+            }
+        } else {
+            $this->connection->insert($this->options['role_table_name'], array('name' => $name, 'parent_id' => $parentId));
+        }
 
         return $name;
     }
